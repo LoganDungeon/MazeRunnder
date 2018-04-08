c312b53155438dbdbfdeb57b0addd127b8b9f547
LoganDungeon <JonasW96@gmx.net>
2018-04-08 20:46:47 +0200

    Initial folder structure Added initial folder structure and some code for tile and world

---
+++ Assets/Scripts/Models/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class Tile
+{
+	// Coordinates of the tile
+	public int X
+	{
+		get;
+		protected set;
+	}
+
+	public int Z
+	{
+		get;
+		protected set;
+	}
+
+	public Tile(int x, int z)
+	{
+		this.X = x;
+		this.Z = z;
+	}
+}
---
+++ Assets/Scripts/Models/World.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World
+{
+
+	// Dimensions of the world
+	public int Width // X direction
+	{
+		get;
+		protected set;
+	}
+
+	public int Height // Z direction
+	{
+		get;
+		protected set;
+	}
+
+	private Tile[,] tiles;
+
+	public World(int width, int height)
+	{
+		this.Width = width;
+		this.Height = height;
+
+		this.tiles = new Tile[width, height];
+
+		for(int x = 0; x < width; x++)
+		{
+			for(int z = 0; z < height; z++)
+			{
+				this.tiles[x, z] = new Tile(x, z);
+			}
+		}
+	}
+
+	public Tile GetTileAt(int x, int z)
+	{
+		return tiles[x, z];
+	}
+
+	public Tile GetTileAt(float x, float z)
+	{
+		return tiles[Mathf.FloorToInt(x), Mathf.FloorToInt(z)];
+	}
+}
